149c30428a59858df27edaca10d99d7f1b5db382
Diana <mada.chirica@gmail.com>
2025-01-30 21:26:44 +0000

    feat: google auth working

--- src/app/auth /useAuth.ts
+++ src/app/auth /useAuth.ts
@@ -1,9 +1,12 @@
+"use client"
 import { useEffect, useState } from "react"
 import { supabase } from "../api/supabase/supabase"
+import { useRouter } from "next/navigation"
 
 export function useAuth() {
   const [user, setUser] = useState<any>(null)
   const [loading, setLoading] = useState(true)
+  const router = useRouter()
 
   useEffect(() => {
     // Check user session
@@ -13,6 +16,9 @@ export function useAuth() {
       } = await supabase.auth.getUser()
       setUser(user)
       setLoading(false)
+      if (user) {
+        router.push("/quiz")
+      }
     }
 
     getUser()
@@ -20,6 +26,9 @@ export function useAuth() {
     // Listen for auth changes
     const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
       setUser(session?.user ?? null)
+      if (session?.user) {
+        router.push("/quiz")
+      }
     })
 
     return () => authListener.subscription.unsubscribe()
---
+++ src/components/navBar/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { logout } from "../../app/api/supabase/util"
+import { useAuth } from "../../app/auth /useAuth"
+import styles from "./nav.module.css"
+
+export default function Navbar() {
+  const { user } = useAuth()
+  console.log({ user })
+
+  return (
+    <nav className={styles.navBox}>
+      <h1>Quizardry</h1>
+      <div>
+        {user ? (
+          <div>
+            <span>Welcome, {user.email}</span>
+            <button onClick={logout}>Logout</button>
+          </div>
+        ) : (
+          <span>Please use the form bellow to sign in</span>
+        )}
+      </div>
+    </nav>
+  )
+}
