1792b6fcc9dbee29305d221317b2c11b450962a5
Diana <mada.chirica@gmail.com>
2025-02-03 11:59:45 +0000

    refacor: added types for User

--- src/app/auth /useAuth.ts
+++ src/app/auth /useAuth.ts
@@ -2,9 +2,10 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../api/supabase/supabase"
 import { useRouter } from "next/navigation"
+import { User } from "@supabase/supabase-js"
 
 export function useAuth() {
-  const [user, setUser] = useState<any>(null)
+  const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
   const router = useRouter()
 
@@ -17,6 +18,7 @@ export function useAuth() {
       setUser(user)
       setLoading(false)
       if (user) {
+        console.log({ user })
         router.push("/quiz")
       }
     }
@@ -34,7 +36,7 @@ export function useAuth() {
     })
 
     return () => authListener.subscription.unsubscribe()
-  }, [])
+  }, [router])
 
   return { user, loading }
 }
